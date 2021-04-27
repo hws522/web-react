7797f711b43e53453eeb84f3b410562c2467776a
hws522 <sraphic@kakao.com>
2021-04-27 23:00:48 +0900

    create first component for pure.html

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,56 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello, React!!</div>;
+class Article extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        World Wide Web!
+      </header>
+    );
+  }
+} // 유사 자바스크립트이다. 자바스크립트가 아니기 때문에 콘솔창에서는 에러가 뜸. 규칙이 잘 지켜지지 않은 상태다.
+// jsx 인데 이렇게 만든 소스코드를 create-react-app 이 javascript 로 컨버팅해준다고 생각하면 된다.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Nav></Nav>
+        <Article></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
