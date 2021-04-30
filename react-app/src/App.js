7aa6d7b5cf24b9678b3685e42050c01253b1de28
hws522 <sraphic@kakao.com>
2021-05-01 00:28:05 +0900

    component division

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,45 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-
-class Article extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
-class Nav extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-} // 유사 자바스크립트이다. 자바스크립트가 아니기 때문에 콘솔창에서는 에러가 뜸. 규칙이 잘 지켜지지 않은 상태다.
+import Nav from "./components/nav";
+import Article from "./components/article";
+import Subject from "./components/subject";
+// 유사 자바스크립트이다. 자바스크립트가 아니기 때문에 콘솔창에서는 에러가 뜸. 규칙이 잘 지켜지지 않은 상태다.
 // jsx 인데 이렇게 만든 소스코드를 create-react-app 이 javascript 로 컨버팅해준다고 생각하면 된다.
 class App extends Component {
   render() {
